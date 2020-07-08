4b9a28cfcd93eccae9c0faf6d678578636f8a8df
Lee-Ben <blee3@ualberta.ca>
2020-07-08 10:24:11 -0600

    test ignore

--- Data_Plotting/compare_data.m
+++ Data_Plotting/compare_data.m
@@ -14,7 +14,6 @@ function compare_data(d,dpred1,dpred2)
 %
 % (C) 2020 Unsworth Research Group (University of Alberta, Edmonton, Canada)
 %%
-% resolved comments...?
 G = load_geoboundary_file_list;
 
 is = 1; check = 0;
