effb20dd399209e6a12de7436e5f4289dea8153c
darcycordell <dcordell@ualberta.ca>
2020-07-08 14:03:24 -0600

    Changed rms labels
    Martyn's Comment (1)
    
    RMS labels added to interpolated % different plots.

--- Data_Plotting/plot_misfit_percent_diff_map.m
+++ Data_Plotting/plot_misfit_percent_diff_map.m
@@ -1,17 +1,22 @@
-function plot_misfit_percent_diff_map(dobs,dpred,ip)
+function plot_misfit_percent_diff_map(dobs,dpred,ip,s)
 % Function which plots the percent difference between observed and
 % predicted MT data as interpolated percent difference of apparent
 % resistivity and phase. 
 %
-% Usage: plot_misfit_percent_diff_map(dobs,dpred,ip)
+% Usage: plot_misfit_percent_diff_map(dobs,dpred,ip,s)
 %
 % "dobs" is the observed MT data structure
 % "dpred" is the predicted MT data structurce
 % "ip" is the index of the period to plot
+% "s" is an OPTIONAL input of a structure of detailed statistics
 %
 
 u = user_defaults;
 [L] = load_geoboundary_file_list;
+
+if ~exist('s','var')
+    s = detailed_statistics(dobs,dpred);
+end
 
 close all
 drho = 100*(abs(dobs.rho - dpred.rho)./dobs.rho); %Calcuate rho % diff
@@ -51,13 +56,13 @@ for i = 1:4 %Loop over components
 
     if dobs.nr == 2 % fix title plotting for off-diagonal only impedances
         if i == 2 || i == 3
-            title([dobs.responses{i-1}(2:end),' App. Res.'])
+            title([dobs.responses{i-1}(2:end),' App. Res. R.M.S. = ',num2str(s.rms_fr(i,ip),'%.2f')])
         else % clear axes for diagonal components
             cla
             colorbar('off')
         end
     else      
-        title([dobs.responses{i}(2:end),' App. Res.'])
+        title([dobs.responses{i}(2:end),' App. Res. R.M.S. = ',num2str(s.rms_fr(i,ip),'%.2f')])
     end            
     
 end
@@ -94,11 +99,6 @@ annotation('textbox', [0 0.9 1 0.08], ...
     'String', ['Interpolated Percent Difference @ T = ',num2str(dobs.T(ip)),' s'], ...
     'EdgeColor', 'none', ...
     'HorizontalAlignment', 'center','FontSize',12,'FontWeight','bold')
-
-
-print_figure(['compare_responses_map_',dpred.niter],['percent_diff_',num2str(dobs.T(ip))]); %Save figure
-
-
 
 
 end
