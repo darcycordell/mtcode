58e5a4afbb202a4ba5cb690eb906e43dda79f94c
darcycordell <dcordell@ualberta.ca>
2020-12-03 12:59:36 -0700

    Update plot_diagonal_section.m
    Old version did not allow you to select points outside the model space. This is changed so that you can now do it and there is no error (just a warning displayed).

--- Model_Plotting/plot_diagonal_section.m
+++ Model_Plotting/plot_diagonal_section.m
@@ -81,6 +81,7 @@ if main_menu == 1 %Option to Click Points
     xp = xp*1000;
     yp = yp*1000;
     
+    
 elseif main_menu == 2 % enter X/Y end points manually
     
     prompt={sprintf('Enter in km\n\nNS 1'),'EW 1','NS 2','EW 2'}; 
@@ -91,6 +92,8 @@ elseif main_menu == 2 % enter X/Y end points manually
 
     xp=[str2double(dinp{1});str2double(dinp{3})]; 
     yp=[str2double(dinp{2});str2double(dinp{4})];
+    
+    %yp = [-10, 2.5]; xp = [0, -15]; %#3 Diagonal Slice
 
     %These are the chosen points
     xp = xp*1000;
@@ -193,182 +196,183 @@ end
 [xp, id] = sort(xp,'descend');
 yp = yp(id);
 %%
+
 if max(xp)>max(m.cx) || min(xp)<min(m.cx) || max(yp)>max(m.cy) || min(yp)<min(m.cy)
-    disp('Error: Your lat/long ranges are outside the model space')
+     disp('Note: Your lat/long ranges are outside the model space')
+end
+
+%Step 1: Find the slope of the line and intercept of the line between the
+%two points
+
+s=(xp(2)-xp(1))/(yp(2)-yp(1)); %slope
+b=xp(1)-s*yp(1); %intercept
+
+if isinf(s)
+    disp('The diagonal selection does not work for NS-profiles. Choose to plot NS section in S3 instead');
+    return;
+end
+
+
+%Step 2: Find the intersections between the line and the grid edges in NS
+%and EW directions
+
+%EW first (xint: dealing with known east-west points)
+int=[];
+for k=1:m.ny
+    xint=s*m.cy(k)+b; %slope formula to find intersections with known x
+    if m.cy(k)>=min(yp) && m.cy(k)<=max(yp)
+        int=[int; [m.cy(k) xint]];
+    end
+end
+
+%NS second (xint: dealing with known north-south points)
+for k=1:m.nx
+    yint=(m.cx(k)-b)/s; %slope formula to find intersections with known y
+    if m.cx(k)>=min(xp) && m.cx(k)<=max(xp)
+        int=[int; [yint m.cx(k)]];
+    end
+end
+
+%add beginning and end points to intercept matrix and sort
+int=[int;[yp(1) xp(1)];[yp(2) xp(2)]];
+
+%Sort the intercepts so that profile is always plotted the same way regardless
+%of the slope of the line. It is plotted such that the WEST side of
+%the profie is on the LEFT and the EAST side of the profile is on the
+%RIGHT. Similarly, the NORTH side of the profile is on the LEFT and the
+%SOUTH side of the profile is on the RIGHT.
+[int, idx] =sortrows(unique(int,'rows'),2); %remove any duplicates
+if s<0 %For sections with negative slopes (i.e. NW-SE)
+    int = int(idx,:);
 else
-
-    %Step 1: Find the slope of the line and intercept of the line between the
-    %two points
-
-    s=(xp(2)-xp(1))/(yp(2)-yp(1)); %slope
-    b=xp(1)-s*yp(1); %intercept
-
-    if isinf(s)
-        disp('The diagonal selection does not work for NS-profiles. Choose to plot NS section in S3 instead');
-        return;
-    end
-
-
-    %Step 2: Find the intersections between the line and the grid edges in NS
-    %and EW directions
-
-    %EW first (xint: dealing with known east-west points)
-    int=[];
-    for k=1:m.ny
-        xint=s*m.cy(k)+b; %slope formula to find intersections with known x
-        if m.cy(k)>=min(yp) && m.cy(k)<=max(yp)
-            int=[int; [m.cy(k) xint]];
-        end
-    end
-
-    %NS second (xint: dealing with known north-south points)
-    for k=1:m.nx
-        yint=(m.cx(k)-b)/s; %slope formula to find intersections with known y
-        if m.cx(k)>=min(xp) && m.cx(k)<=max(xp)
-            int=[int; [yint m.cx(k)]];
-        end
-    end
-
-    %add beginning and end points to intercept matrix and sort
-    int=[int;[yp(1) xp(1)];[yp(2) xp(2)]];
-    
-    %Sort the intercepts so that profile is always plotted the same way regardless
-    %of the slope of the line. It is plotted such that the WEST side of
-    %the profie is on the LEFT and the EAST side of the profile is on the
-    %RIGHT. Similarly, the NORTH side of the profile is on the LEFT and the
-    %SOUTH side of the profile is on the RIGHT.
-    [int, idx] =sortrows(unique(int,'rows'),2); %remove any duplicates
-    if s<0 %For sections with negative slopes (i.e. NW-SE)
+    if s>=1 %For sections with positive slopes >45 degrees (i.e. steep NE-SW)
+        int = int(flipud(idx),:); 
+    else %For sections with positive slopes <45 degrees (i.e. shallow NE-SW)
         int = int(idx,:);
-    else
-        if s>=1 %For sections with positive slopes >45 degrees (i.e. steep NE-SW)
-            int = int(flipud(idx),:); 
-        else %For sections with positive slopes <45 degrees (i.e. shallow NE-SW)
-            int = int(idx,:);
-        end
     end
-    
-    %Option to plot the intercepts on the map (for debugging purposes)
+end
+
+%Option to plot the intercepts on the map (for debugging purposes)
 %     plot(int(:,1)/1000,int(:,2)/1000,'*w')
 %     plot(int(1,1)/1000,int(1,2)/1000,'*k','MarkerSize',12)
 
-    %Step 3: Find the distance and midpoint of each line segment through each grid cell
+%Step 3: Find the distance and midpoint of each line segment through each grid cell
 
-    %Get lengths and midpoints of line segments
-    L=zeros(length(int(:,1))-1,1);L_mid=zeros(length(L),2); L_d = zeros(size(L));
-    for k=1:length(int)-1 %loops over all grid intersections
-        L_set=sqrt((int(k+1,1)-int(k,1))^2+(int(k+1,2)-int(k,2))^2);
-        L(k)=L_set;
-        L_mid_set=(int(k+1,:)+int(k,:))./2;
-        L_mid(k,:)=L_mid_set;
+%Get lengths and midpoints of line segments
+L=zeros(length(int(:,1))-1,1);L_mid=zeros(length(L),2); L_d = zeros(size(L));
+for k=1:length(int)-1 %loops over all grid intersections
+    L_set=sqrt((int(k+1,1)-int(k,1))^2+(int(k+1,2)-int(k,2))^2);
+    L(k)=L_set;
+    L_mid_set=(int(k+1,:)+int(k,:))./2;
+    L_mid(k,:)=L_mid_set;
 
-        L_d(k) = L_set;
+    L_d(k) = L_set;
 
-    end
+end
 
-    dist=cumsum(L_d);
+dist=cumsum(L_d);
 
-    %Step 4: Find which cell the midpoint belongs in
-    x_grid = zeros(length(L),1); y_grid = zeros(length(L),1);
-    for j=1:length(L)
-        y_grid(j) = nearestpoint(L_mid(j,1),m.cy);
-        x_grid(j) = nearestpoint(L_mid(j,2),m.cx);
-    end
-    
-    %Option to plot the grid cells on the map (for debugging purposes)
+%Step 4: Find which cell the midpoint belongs in
+x_grid = zeros(length(L),1); y_grid = zeros(length(L),1);
+for j=1:length(L)
+    y_grid(j) = nearestpoint(L_mid(j,1),m.cy);
+    x_grid(j) = nearestpoint(L_mid(j,2),m.cx);
+end
+
+%Option to plot the grid cells on the map (for debugging purposes)
 %     plot(m.cy(y_grid)/1000,m.cx(x_grid)/1000,'xk')
 %     plot(m.cy(y_grid(1))/1000,m.cx(x_grid(1))/1000,'xr','MarkerSize',12)
 %%
-    res_plot=zeros(m.nz,length(L));
-    for j=1:length(L)
-        res_plot(:,j) = m.A(x_grid(j),y_grid(j),:); %pulls slice model cells from A matrix
+res_plot=zeros(m.nz,length(L));
+for j=1:length(L)
+    res_plot(:,j) = m.A(x_grid(j),y_grid(j),:); %pulls slice model cells from A matrix
+end
+
+if strcmp(u.diagonal_section_mode,'interp')
+    ind = find(isnan(res_plot));
+    res_plot = inpaint_nans(res_plot);
+    px = cumsum(ones(length(L),1)*sum(L)/(length(L)));
+    [D,Zorig] = meshgrid(dist,m.cz);
+    [P,Z] = meshgrid(px,m.cz);
+    res_plot =interp2(D,Zorig,res_plot,P,Z);
+    res_plot(ind) = NaN;
+    dist = px;
+end
+%%
+if ~strcmp(d.site,'None')
+    %Project sites within the tolerance distance from the profile onto the
+    %profile
+    k=0; id = []; d_elev = [];
+    for i = 1:length(d.idx)
+
+
+            %Find the minimum distance between the site and the profile cell
+            %centers. This is the cell center which the site will be
+            %projected to
+            [d_site, ind] = min(sqrt((m.cx(d.idx(i))-L_mid(:,2)).^2+(m.cy(d.idy(i))-L_mid(:,1)).^2));
+
+            if d_site<u.tol*1000 %if station is less than tolerance (km) from profile then include it on the profile plot
+                k=k+1;
+                id(k) = i; %Projected station index
+
+                %d_elev includes the model indices of the projected
+                %site and the distance the site is along the profile
+                d_elev(k,:) = [x_grid(ind) y_grid(ind) dist(ind)];
+            end
+
     end
-        
-    if strcmp(u.diagonal_section_mode,'interp')
-        ind = find(isnan(res_plot));
-        res_plot = inpaint_nans(res_plot);
-        px = cumsum(ones(length(L),1)*sum(L)/(length(L)));
-        [D,Zorig] = meshgrid(dist,m.cz);
-        [P,Z] = meshgrid(px,m.cz);
-        res_plot =interp2(D,Zorig,res_plot,P,Z);
-        res_plot(ind) = NaN;
-        dist = px;
-    end
-    %%
-    if ~strcmp(d.site,'None')
-        %Project sites within the tolerance distance from the profile onto the
-        %profile
-        k=0; id = []; d_elev = [];
-        for i = 1:length(d.idx)
-            
 
-                %Find the minimum distance between the site and the profile cell
-                %centers. This is the cell center which the site will be
-                %projected to
-                [d_site, ind] = min(sqrt((m.cx(d.idx(i))-L_mid(:,2)).^2+(m.cy(d.idy(i))-L_mid(:,1)).^2));
+    %Plot which sites will be projected to the profile
+    figure(1)
+    plot(d.y(id)/1000,d.x(id)/1000,'r.','markersize',14);   
+    %Option to plot the cell centers which the stations are projected to
+    %plot(m.cy(d_elev(:,2))/1000,m.cx(d_elev(:,1))/1000,'xr')
 
-                if d_site<u.tol*1000 %if station is less than tolerance (km) from profile then include it on the profile plot
-                    k=k+1;
-                    id(k) = i; %Projected station index
-                    
-                    %d_elev includes the model indices of the projected
-                    %site and the distance the site is along the profile
-                    d_elev(k,:) = [x_grid(ind) y_grid(ind) dist(ind)];
-                end
-            
-        end
-        
-        %Plot which sites will be projected to the profile
-        figure(1)
-        plot(d.y(id)/1000,d.x(id)/1000,'r.','markersize',14);   
-        %Option to plot the cell centers which the stations are projected to
-        %plot(m.cy(d_elev(:,2))/1000,m.cx(d_elev(:,1))/1000,'xr')
-  
-    end
-    
-    zind = 1:nearestpoint(u.zmax*1000,m.cz);
+end
 
-    %Plot section
-    figure(2);
-   
-    plot_cross_section(dist/1000,m.cz(zind)/1000,res_plot(zind,:)'); % transpose to negate transpose in this function...
-    
-    xlabel(['Kilometers from (',num2str(xp(1)/1000),' NS, ',num2str(yp(1)/1000),' EW) to (',num2str(xp(2)/1000),' NS, ',num2str(yp(2)/1000),' EW)']);
-    ylabel('Depth Below Sealevel (km)')
-    title('Diagonal Slice Through Model');
+zind = 1:nearestpoint(u.zmax*1000,m.cz);
 
-    %%
+%Plot section
+figure(2);
+
+plot_cross_section(dist/1000,m.cz(zind)/1000,res_plot(zind,:)'); % transpose to negate transpose in this function...
+
+xlabel(['Kilometers from (',num2str(xp(1)/1000),' NS, ',num2str(yp(1)/1000),' EW) to (',num2str(xp(2)/1000),' NS, ',num2str(yp(2)/1000),' EW)']);
+ylabel('Depth Below Sealevel (km)')
+title('Diagonal Slice Through Model');
+
+%%
 %     seisy = dist/1000;
 %     seisz = m.cz(zind)/1000;
 %     seisA = res_plot(zind,:);
 %     save seis_2d_for_interpolation_v2 seisy seisz seisA
-    
-    %Plot geoboundary file on diagonal section (not yet implemented)
-    plot_geoboundaries_diagonal_section(d,L_mid(:,1),L_mid(:,2))    
-    
-    %Plot projected station locations (projected onto the profile and
-    %projected onto the elevation surface of the profile)
-    if ~strcmp(d.site,'None')
-        if ~isempty(d_elev)
-            figure(2);
-            elev = m.Z(sub2ind(size(m.Z),d_elev(:,1),d_elev(:,2)));
-            plot(d_elev(:,3)/1000,elev/1000 -u.zoff,'vk','MarkerFaceColor','k'); hold on
-        end
+
+%Plot geoboundary file on diagonal section (not yet implemented)
+plot_geoboundaries_diagonal_section(d,L_mid(:,1),L_mid(:,2))    
+
+%Plot projected station locations (projected onto the profile and
+%projected onto the elevation surface of the profile)
+if ~strcmp(d.site,'None')
+    if ~isempty(d_elev)
+        figure(2);
+        elev = m.Z(sub2ind(size(m.Z),d_elev(:,1),d_elev(:,2)));
+        plot(d_elev(:,3)/1000,elev/1000 -u.zoff,'vk','MarkerFaceColor','k'); hold on
     end
-    
-
-    print_figure(['diagonal_slices_',m.niter],['slice_from_',num2str(xp(1)/1000),'_',num2str(yp(1)/1000),'_to_',num2str(xp(2)/1000),'_',num2str(yp(2)/1000)])
-    
-    figure(1)
-    print_figure(['diagonal_slices_',m.niter],['slice_from_',num2str(xp(1)/1000),'_',num2str(yp(1)/1000),'_to_',num2str(xp(2)/1000),'_',num2str(yp(2)/1000),'_MAP']) 
-
-
-    %Output x,y,z,rho which is the x,y,z coordinates and corresponding rho
-    %values of the plotted section
-    x = L_mid(:,2)/1000;
-    y = L_mid(:,1)/1000;
-    z = m.cz(zind)/1000;
-    rho = res_plot(zind,:)';
-
 end
+
+
+print_figure(['diagonal_slices_',m.niter],['slice_from_',num2str(xp(1)/1000),'_',num2str(yp(1)/1000),'_to_',num2str(xp(2)/1000),'_',num2str(yp(2)/1000)])
+
+figure(1)
+print_figure(['diagonal_slices_',m.niter],['slice_from_',num2str(xp(1)/1000),'_',num2str(yp(1)/1000),'_to_',num2str(xp(2)/1000),'_',num2str(yp(2)/1000),'_MAP']) 
+
+
+%Output x,y,z,rho which is the x,y,z coordinates and corresponding rho
+%values of the plotted section
+x = L_mid(:,2)/1000;
+y = L_mid(:,1)/1000;
+z = m.cz(zind)/1000;
+rho = res_plot(zind,:)';
+
+
 end %End function
