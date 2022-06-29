9978dfe29857f79fd7147559e7ba94b50f97594e
darcycordell <dcordell@ualberta.ca>
2022-06-29 10:28:11 -0600

    Update compare_data.m

--- Data_Plotting/compare_data.m
+++ Data_Plotting/compare_data.m
@@ -16,6 +16,9 @@ function compare_data(d,dpred1,dpred2)
 %%
 G = load_geoboundary_file_list;
 
+s1 = detailed_statistics(d,dpred1);
+s2 = detailed_statistics(d,dpred2);
+
 is = 1; check = 0;
 while 1
     
@@ -30,33 +33,34 @@ while 1
         geoshow(d.loc(is,1),d.loc(is,2),'DisplayType','point','Marker','.','MarkerEdgeColor','r','MarkerSize',15)
         axis(d.lim)
         xlabel('Longitude'); ylabel('Latitude')
+        title(['dpred1 rms = ',num2str(s1.rms_site(is)),'. dpred2 rms = ',num2str(s2.rms_site(is))])
 
         plot_rho_pha(d,is); hold on %Plot observed data
         plot_rho_pha(dpred1,is); %Plot predicted data #1
         
         %Plot predicted data #2 with a different linestyle and linewidth
-        subplot(2,3,1) %Diagonal rho
+        ax(1) = subplot(2,3,1); %Diagonal rho
         h = logerrorbar(dpred2.T,dpred2.rho(:,1,is),dpred2.rhoerr(:,1,is),'--m','-r');hold on; set(h,'LineWidth',2)
         h = logerrorbar(dpred2.T,dpred2.rho(:,4,is),dpred2.rhoerr(:,4,is),'--g','-b'); set(h,'LineWidth',2)
         manual_legend('XX Predicted Data #1','-m','YY Predicted Data #1','-g','XX Predicted Data #2','--m','YY Predicted Data #2','--g');
         
-        subplot(2,3,2) %Off diagonal rho
+        ax(2) = subplot(2,3,2); %Off diagonal rho
         h = logerrorbar(dpred2.T,dpred2.rho(:,2,is),dpred2.rhoerr(:,2,is),'--r','-r');hold on; set(h,'LineWidth',2)
         h = logerrorbar(dpred2.T,dpred2.rho(:,3,is),dpred2.rhoerr(:,3,is),'--b','-b'); set(h,'LineWidth',2)
         manual_legend('XY Predicted Data #1','-r','YX Predicted Data #1','-b','XY Predicted Data #2','--r','YX Predicted Data #2','--b');
         
-        subplot(2,3,4); %Diagonal phase
+        ax(3) = subplot(2,3,4); %Diagonal phase
         semilogx(dpred2.T,dpred2.pha(:,1,is),'--m','LineWidth',2); hold on
         semilogx(dpred2.T,dpred2.pha(:,4,is)+180,'--g','LineWidth',2);
         
-        subplot(2,3,5); %Off diagonal phase
+        ax(4) = subplot(2,3,5); %Off diagonal phase
         semilogx(dpred2.T,dpred2.pha(:,2,is),'--r','LineWidth',2); hold on
         semilogx(dpred2.T,dpred2.pha(:,3,is)+180,'--b','LineWidth',2);
 
         print_figure('compare_rho_pha',[d.site{is},'_d_',d.name,'_dpred1_',dpred1.name,'_dpred2_',dpred2.name]); %Save figure
     end
     
-    site_menu = menu('','Next Site','Previous Site','Select Site from List','Plot RMS Misfit Map Comparison','Return');
+    site_menu = menu('','Next Site','Previous Site','Select Site from List','Select Site from Map','Plot RMS Misfit Map Comparison','Return');
     
     if site_menu == 1 %Advance through stations
         is = is+1; check = 0;
@@ -78,7 +82,36 @@ while 1
         if isempty(is)
             break
         end
-    elseif site_menu == 4 %Compute rms misfit map
+        
+    elseif site_menu == 4 %Select site from map
+        
+        [X, Y] = ginput_editdata(1);%get x,y points on either phase or rho axes       
+
+        clickedAx=gca; %We have to figure out which axis the user clicked on to make sure they clicked on the map
+        if clickedAx==ax(1) || clickedAx == ax(2) || clickedAx == ax(3) || clickedAx == ax(4)
+            subplot(2,3,[3 6]); title('Click on the map to select a site to plot'); 
+        else
+            subplot(2,3,[3 6]); title('');
+
+            %Find the distance from the clicked point to the sites and find
+            %minimum
+            [S] = distance(Y,X,d.loc(:,1),d.loc(:,2),6371000);
+            [~,is] = min(S);
+
+            %Plot selected site
+            set_figure_size(1);
+            plot_rho_pha(d,is);
+            %Plot station locations in map view
+            subplot(2,3,[3 6])
+            geoshow(d.loc(:,1),d.loc(:,2),'DisplayType','point','Marker','.','MarkerEdgeColor','k'); hold on
+            geoshow(d.loc(is,1),d.loc(is,2),'DisplayType','point','Marker','.','MarkerEdgeColor','r','MarkerSize',15)
+            axis(d.lim);
+            xlabel('Longitude'); ylabel('Latitude') 
+            title(['dpred1 rms = ',num2str(s1.rms_site(is)),'. dpred2 rms = ',num2str(s2.rms_site(is))])
+            
+        end
+
+    elseif site_menu == 5 %Compute rms misfit map
         
         s1 = detailed_statistics(d,dpred1);
         s2 = detailed_statistics(d,dpred2);
